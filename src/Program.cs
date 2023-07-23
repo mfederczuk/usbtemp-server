efb2bebdcba91ae1798f54785010d6f57de81949
Michael Federczuk <federczuk.michael@protonmail.com>
2023-07-23 14:05:45 +0200

    Add build files

---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023 Michael Federczuk
+ *
+ * SPDX-License-Identifier: MPL-2.0 AND Apache-2.0
+ */
+
+namespace UsbtempServer;
+
+public static class Program
+{
+	public static int Main(string[] args)
+	{
+		// TODO
+		return 0;
+	}
+}
