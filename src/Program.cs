8753b7e66b3ba29dfef3235546c86e4d7513f165
Michael Federczuk <federczuk.michael@protonmail.com>
2023-07-23 15:55:18 +0200

    Prompt for port name and display thermometer info

--- src/Program.cs
+++ src/Program.cs
@@ -4,13 +4,34 @@
  * SPDX-License-Identifier: MPL-2.0 AND Apache-2.0
  */
 
+using System;
+
 namespace UsbtempServer;
 
 public static class Program
 {
 	public static int Main(string[] args)
 	{
-		// TODO
+		Console.Error.Write("Enter the port name of the USB-thermometer: ");
+		string? portName = Console.ReadLine();
+
+		if ((portName is null) || (portName == string.Empty))
+		{
+			string prefix = ((portName is null) ? "\n" : "");
+			Console.Error.WriteLine(prefix + "Aborted.");
+
+			return 1;
+		}
+
+		using (Thermometer thermometer = Thermometer.OpenNew(portName))
+		{
+			string serialNumber = thermometer.ReadSerialNumber();
+			Temperature initialTemperature = thermometer.ReadTemperature();
+
+			Console.Error.WriteLine($"\nSerial number: {serialNumber}");
+			Console.Error.WriteLine($"Initial temperature: {initialTemperature.ToDegreeCelsiusFloat()}°C");
+		}
+
 		return 0;
 	}
 }
