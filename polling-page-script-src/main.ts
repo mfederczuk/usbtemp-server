e0bc2cebe6d01dbf64b8d76a1775193ba8ea4691
Michael Federczuk <federczuk.michael@protonmail.com>
2023-09-06 16:13:56 +0200

    Pass configured polling interval to the handler

--- polling-page-script-src/main.ts
+++ polling-page-script-src/main.ts
@@ -2,15 +2,30 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
+import { PageConfiguration } from "./configuration";
 import { PollingHandler } from "./polling";
 
-const pollingHandler = new PollingHandler(window, console, 5000);
+let pollingHandler: PollingHandler | null = null;
 
-window.addEventListener("load", () => {
+window.addEventListener("load", (event: Event) => {
+	console.debug(`Window event "${event.type}"`);
+	const pageConfiguration: PageConfiguration =
+		PageConfiguration.fromUrlSearchParams(
+			new URLSearchParams(window.location.search),
+			console,
+		);
+
+	pollingHandler = new PollingHandler(window, console, pageConfiguration.pollingIntervalMs);
 	pollingHandler.start();
 });
 
-document.addEventListener("visibilitychange", () => {
+document.addEventListener("visibilitychange", (event: Event) => {
+	console.debug(`Document event "${event.type}"`);
+	if (pollingHandler === null) {
+		console.error(`Document event "${event.type}": Polling handler is null`);
+		return;
+	}
+
 	if (document.visibilityState === "visible") {
 		pollingHandler.start();
 	} else {
