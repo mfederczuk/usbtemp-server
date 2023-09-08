b7deed78426457b711c078bb36872d99aba71c0b
Michael Federczuk <federczuk.michael@protonmail.com>
2023-09-08 10:41:08 +0200

    Use the class `DiComponent` in the file `main.ts`

--- polling-page-script-src/main.ts
+++ polling-page-script-src/main.ts
@@ -2,41 +2,18 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
-import type { ApiService } from "./api/ApiService";
-import { WindowFetchApiService } from "./api/WindowFetchApiService";
-import { PageConfiguration } from "./configuration";
-import { PollingHandler } from "./polling";
-import { IntlTemperatureFormatter } from "./thermology/IntlTemperatureFormatter";
-import type { TemperatureFormatter } from "./thermology/TemperatureFormatter";
-import type { ActionScheduler } from "./utils/actionScheduling/ActionScheduler";
-import { WindowTimeoutActionScheduler } from "./utils/actionScheduling/WindowTimeoutActionScheduler";
-import { ConsoleLogger } from "./utils/logging/ConsoleLogger";
-import type { Logger } from "./utils/logging/Logger";
+import { DiComponent } from "./DiComponent";
+import type { PollingHandler } from "./polling";
 
 window.addEventListener("load", () => {
-	const logger: Logger = new ConsoleLogger(console);
-
-	const actionScheduler: ActionScheduler = new WindowTimeoutActionScheduler(window);
-
-	const apiService: ApiService = new WindowFetchApiService(window);
-
-	const pageConfiguration: PageConfiguration =
-		PageConfiguration.fromUrlSearchParams(
-			new URLSearchParams(window.location.search),
-			logger,
-		);
-
-	const temperatureFormatter: TemperatureFormatter = new IntlTemperatureFormatter(new Intl.Locale("en-US"));
-
-	const pollingHandler =
-		new PollingHandler(
-			actionScheduler,
-			apiService,
+	const diComponent =
+		new DiComponent(
 			window,
-			logger,
-			pageConfiguration.pollingInterval,
-			temperatureFormatter,
+			console,
+			new Intl.Locale("en-US"),
 		);
+
+	const pollingHandler: PollingHandler = diComponent.getPollingHandler();
 
 	pollingHandler.start();
 
