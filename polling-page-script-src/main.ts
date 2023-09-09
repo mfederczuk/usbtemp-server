98e742ce940044036a1958d836dc56c0b50e5750
Michael Federczuk <federczuk.michael@protonmail.com>
2023-09-09 15:47:14 +0200

    Improve main initialization logic

--- polling-page-script-src/main.ts
+++ polling-page-script-src/main.ts
@@ -1,11 +1,13 @@
 /*
- * SPDX-License-Identifier: CC0-1.0
+ * Copyright (c) 2023 Michael Federczuk
+ *
+ * SPDX-License-Identifier: MPL-2.0 AND Apache-2.0
  */
 
 import { DiComponent } from "./DiComponent";
 import type { PollingHandler } from "./polling";
 
-window.addEventListener("load", () => {
+const setUpPage = (): void => {
 	const diComponent =
 		new DiComponent(
 			window,
@@ -17,11 +19,38 @@ window.addEventListener("load", () => {
 
 	pollingHandler.start();
 
-	document.addEventListener("visibilitychange", () => {
+	window.document.addEventListener("visibilitychange", () => {
 		if (document.visibilityState === "visible") {
 			pollingHandler.start();
 		} else {
 			pollingHandler.stop();
 		}
 	});
-});
+};
+
+if ((window.document.readyState === "interactive") || (window.document.readyState === "complete")) {
+	setUpPage();
+} else {
+	let isPageSetUp: boolean = false;
+
+	const ensurePageIsSetUp = (): void => {
+		if (isPageSetUp) {
+			return;
+		}
+
+		setUpPage();
+		isPageSetUp = true;
+	};
+
+	const handler = (): void => {
+		if ((window.document.readyState !== "interactive") && (window.document.readyState !== "complete")) {
+			return;
+		}
+
+		ensurePageIsSetUp();
+
+		window.document.removeEventListener("readystatechange", handler);
+	};
+
+	window.document.addEventListener("readystatechange", handler);
+}
